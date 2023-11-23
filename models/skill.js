201e178c93c9f0b89f3221e55b9488e79143243b
tworem23 <josh.remington1997@gmail.com>
2023-11-22 18:07:36 -0800

    finish main part of part 1

---
+++ models/skill.js
@@ -0,0 +1,13 @@
+const skills = [
+    {id: 345, skill: 'JavaScript', level: 10},
+    {id: 346, skill: 'HTML', level: 10},
+    {id: 347, skill: 'CSS', level: 10} 
+]
+
+module.exports ={
+    getAll
+}
+
+function getAll() {
+    return skills;
+}
--- routes/skills.js
+++ routes/skills.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var skillsCtrl = require('../controllers/skills')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', skillsCtrl.index);
 
 module.exports = router;
